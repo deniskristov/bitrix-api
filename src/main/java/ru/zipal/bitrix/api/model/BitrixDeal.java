9808deda83ad86d1505e0db04823b88f36ac7000
Denis Kristov <denis.kristov@gmail.com>
2019-12-18 18:42:31 +0200

    Added custom deal fields

--- src/main/java/ru/zipal/bitrix/api/model/BitrixDeal.java
+++ src/main/java/ru/zipal/bitrix/api/model/BitrixDeal.java
@@ -20,8 +20,71 @@ public class BitrixDeal implements HasId {
     private Date closeDate;
     @FieldName("TITLE")
     private String title;
+    @FieldName("CURRENCY_ID")
+    private String currencyId;
+    @FieldName("UF_CRM_1568101160")
+    private String fio;
+    @FieldName("UF_CRM_1568037715")
+    private String documentId;
+    @FieldName("UF_CRM_1568100533")
+    private Integer fineDays;
+    @FieldName("UF_CRM_5D53C3D8264DE")
+    private String productName;
+    // Мобилки, компьютеры и тд
+    @FieldName("UF_CRM_5D5CEE2FA17D2")
+    private String productType;
+    @FieldName("UF_CRM_1570197329")
+    private String phoneNumber;
+    // Город, где расположено отделение
+    @FieldName("UF_CRM_5D53C3D8012F2")
+    private String city;
     @FieldName("COMMENTS")
     private String comment;
     @FieldName("CONTACT_ID")
     private Long contactId;
+    //Новый договор – ‘PROPOSAL’
+    //Просрочен – ‘NEGOTIATION’
+    //Проценты вручн. –‘ 1’
+    //Выкуплен – ‘WON’
+    //Продан – ‘LOSE’
+    @FieldName("STAGE_ID")
+    private String stageId;
+    @FieldName("SOURCE_ID")
+    @Builder.Default
+    private String sourceId = "PARTNER";
+
+    // Money fields
+    @FieldName("UF_CRM_1568037628")
+    private String loan;
+    // Total to pay today = loan + pct + fine
+    @FieldName("OPPORTUNITY")
+    private String opportunity;
+    @FieldName("UF_CRM_1574242509")
+    private String pct;
+    @FieldName("UF_CRM_1574242554")
+    private String fine;
+
+
+    public enum Currency {
+        UAH("UAH");
+
+        private String currency;
+
+        Currency(String currency) {
+            this.currency = currency;
+        }
+    }
+
+    public static class BitrixDealBuilder {
+        public BitrixDealBuilder createNewDeal() {
+            this.stageId = "PROPOSAL";
+            return this;
+        }
+
+        public BitrixDealBuilder createFineDeal(int fineDays) {
+            this.fineDays = fineDays;
+            this.stageId = "NEGOTIATION";
+            return this;
+        }
+    }
 }
